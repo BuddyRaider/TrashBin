769b69ed9c028c74d24582a4c21a3decb23f00bd
BuddyRaider <104165073+BuddyRaider@users.noreply.github.com>
2022-05-06 15:08:15 +0000

    Added index.js and install.sh

---
+++ index.js
@@ -0,0 +1,38 @@
+num = 100;
+
+function toBin(x) {
+	x = parseInt(x);
+	let y = x;
+	let s = "";
+	let z;
+	while (y > 0) {
+		z = Math.floor(y % 2);
+		y = Math.floor(y / 2);
+		s = z.toString() + s;
+	}
+	return s;
+	// alternate [...s].reverse().join("")
+}
+
+function fromBin(x) {
+	x = x.toString().replace(/^0+/g, "");
+	let l = x.split("");
+	const lenL = l.length;
+	let y = 0;
+	for (i=0; i<lenL; i++) {
+		y = Math.floor((2 * y) + parseInt(l[i]));
+	}
+	return y;
+}
+
+function bitShift(x, left=false) {
+	x = x.toString().replace(/^0+/g, "").split("");
+	if (left) {
+		x.push(x.shift());
+	} else {
+		x.unshift(x.pop());
+	}
+	return x.join("");
+}
+
+console.log(fromBin(bitShift(toBin(11))))
